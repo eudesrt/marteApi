bbb6d74c05939922e88ffaa6339735c93d4f29e1
Eudes <Eudes@DESKTOP-7GCEH41.Home>
2018-07-06 01:46:13 -0300

    Primeiro Comits

---
+++ src/main/java/com/br/marte/api/MarteApiApplication.java
@@ -0,0 +1,12 @@
+package com.br.marte.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarteApiApplication.class, args);
+	}
+}
