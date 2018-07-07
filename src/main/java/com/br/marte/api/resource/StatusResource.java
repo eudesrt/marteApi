fd6787e928ee163c6dab96c3ed551c0d3dd9ba26
Eudes <Eudes@DESKTOP-7GCEH41.Home>
2018-07-06 21:11:02 -0300

    Teste Heroku

--- src/main/java/com/br/marte/api/resource/StatusResource.java
+++ src/main/java/com/br/marte/api/resource/StatusResource.java
@@ -2,13 +2,17 @@ package com.br.marte.api.resource;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.br.marte.api.repository.StatusRepository;
 import com.br.marte.api.model.Status;
+import com.br.marte.api.repository.StatusRepository;
 
 
 @RestController
@@ -16,12 +20,21 @@ import com.br.marte.api.model.Status;
 public class StatusResource {
 	
 	@Autowired
-	private StatusRepository statusRepository;
+	private StatusRepository ws;
 	
 	@GetMapping
 	private List<Status> findAll(){
 		
-		return statusRepository.findAll();		
+		return ws.findAll();		
 	}
+	
+	@PostMapping
+	public Status statusAdd(@Valid @RequestBody Status status) {
+		
+		System.out.println("Entrei em status add ");
+		
+		return ws.save(status);
+	}
+	
 
 }
