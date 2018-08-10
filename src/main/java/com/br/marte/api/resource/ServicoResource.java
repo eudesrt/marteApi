333a758c7508e9ce74bb496845da4826194e6c3c
Your NameRafael <eudesrt@gmail.comm>
2018-08-10 01:55:38 -0300

    Melhorias

--- src/main/java/com/br/marte/api/resource/ServicoResource.java
+++ src/main/java/com/br/marte/api/resource/ServicoResource.java
@@ -3,6 +3,7 @@ package com.br.marte.api.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class ServicoResource {
 	@Autowired
 	private ServicoRepository servicoRepository;
 	
+	@CrossOrigin(maxAge =10 ,origins= {"http://localhost:4200"})
 	@GetMapping
 	public List<Servico> findAll(){
 		return servicoRepository.findAll();
