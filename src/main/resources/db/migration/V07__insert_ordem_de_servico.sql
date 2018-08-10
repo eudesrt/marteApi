4604e1d7be799d6c9fd522d03ca6b0024cc8130c
DESKTOP-F52P6MD\Rafael <eudesrt@gmail.com>
2018-08-10 14:17:00 -0300

    inserte base de teste heroku

---
+++ src/main/resources/db/migration/V07__insert_ordem_de_servico.sql
@@ -0,0 +1,60 @@
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6651,'INCLUIR CONTRATO RETORNO SODEXO','2013-02-08','2013-02-08','2013-02-08',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6649,'INCLUIR CONTRATO RETORNO CITI','2013-02-08','2013-02-08','2013-02-08',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6664,'EXPORTACAO TICKET','2013-02-20','2013-02-20','2013-02-20',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6658,'DETALHES NO PAINEL DE BORDO','2013-02-21','2013-02-21','2013-02-21',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6677,'RELATORIO ESPECIFICO DEVOLUCAO','2013-02-27','2013-02-27','2013-02-27',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6696,'PAINEL DE BORDO (REL. CUSTÓDIA)','2013-02-27','2013-02-27','2013-02-27',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6719,'RELATORIO ESTATISTICO JALL','2013-03-06','2013-03-06','2013-03-06',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6722,'AJUSTE CHANCELA ESD PEGASUS','2013-03-06','2013-03-06','2013-03-06',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6750,'CTT CUSTODIA BV IMPORTACAO','2013-03-14','2013-03-14','2013-03-14',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6751,'EXPORTACAO VISA VALE','2013-03-25','2013-03-25','2013-03-25',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6717,'EVENTOS SINISTRO','2013-04-02','2013-04-02','2013-04-02',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6795,'TELA CONSULTA JALL - INCLUSÃO INFORMAÇÃO','2013-04-09','2013-04-09','2013-04-09',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6809,'EXPORTAÇÃO CITI','2013-04-11','2013-04-11','2013-04-11',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6826,'REL UNIFICADO POR ARQUIVO - COMPLEMENTO INF ITAU','2013-04-17','2013-04-17','2013-04-17',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6833,'ALTERAR RETORNO SODEXO','2013-04-18','2013-04-18','2013-04-18',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6825,'RETORNO VISA VALE','2013-04-18','2013-04-18','2013-04-18',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6844,'COMPLEMENTO CORREIOS PEGASUS','2013-04-19','2013-04-23','2013-04-23',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6857,'ARQUIVO REMESSA CORREIOS','2013-04-19','2013-04-26','2013-04-26',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6838,'IMPORTAÇÃO COLETA CACIQUE','2013-04-18','2013-04-23','2013-04-23',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6859,'111 TICKET REGRA PESO','2013-04-24','2013-04-24','2013-04-24',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6862,'PROCESSADOR CUSTODIA TICK','2013-04-25','2013-04-30','2013-04-30',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6880,'DATA EXPORTACAO TICKET','2013-04-30','2013-05-07','2013-05-07',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6879,'PRODUTO CUSTODIA PORTO SE','2013-04-30','2013-05-09','2013-05-09',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6876,'SMARTNET IMPORTAÇÃO','2013-04-30','2013-05-02','2013-05-02',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6883,'RETORNO RIOCARD INCONSIST','2013-05-02','2013-05-02','2013-05-02',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6900,'BUG RELATORIO PONTUALIDAD','2013-05-09','2013-05-09','2013-05-09',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6907,'BAIRRO RELATORIO UNIFICAD','2013-05-09','2013-05-10','2013-05-10',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6913,'AJUSTES EXPORTACAO SODEXO','2013-05-13','2013-05-13','2013-05-13',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6912,'AJUTES IMPORTACAO SODEXO','2013-05-13','2013-05-14','2013-05-14',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6920,'AJUSTE ARQUIVO XML EXPORTACAO EPR','2013-05-15','2013-05-15','2013-05-15',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6940,'AJUSTE REGRA SODEXO REMES','2013-05-21','2013-05-22','2013-05-22',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6936,'FERRAMENTA VISA MIAMI PESQUISA CEP','2013-05-21','2013-06-07','2013-06-07',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6957,'INCLUIR CONTRATOS QUALICORP','2013-05-28','2013-05-31','2013-05-31',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6955,'CODIGO DE BARRAS AR FLASH','2013-05-27','2013-05-28','2013-05-28',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6980,'VINCULAÇÃO TERCEIRO EM LOTE ERRO','2013-06-12','2012-06-13','2012-06-13',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (6982,'RELATORIO DE MAPA REDESPACHO','2013-06-12','2013-06-21','2013-06-21',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7004,'AJUSTES OBS QUALICORP REMESSA','2013-06-21','2013-06-26','2013-06-26',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7015,'IMPORTACAO QUALICORP','2013-07-02','2013-07-02','2013-07-02',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7049,'ALTERAR ARQUIVO RETORNO ITAU','2013-07-12','2013-07-12','2013-07-12',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7046,'HSBC RETIRADA DA REGRA PREMIER','2013-07-12','2013-07-15','2013-07-15',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7042,'AJUSTE ARQUIVO CORREIOS E','2013-07-12','2013-07-15','2013-07-15',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7053,'AJUSTE RETORNO BONSUCESSO','2013-07-16','2013-07-15','2013-07-15',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7065,'ERRO EXPORTACAO','2013-07-18','2013-07-19','2013-07-19',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7081,'AJUSTE IMPORTACAO QUALICORP','2013-07-25','2013-07-25','2013-07-25',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7094,'AJUSTE IMPORTACAO SODEXO','2013-08-02','2013-08-06','2013-08-06',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7086,'TMK PEGASUS','2012-07-29','2013-08-07','2013-08-07',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7108,'CAMPO DE OBSERVAÇÃO JALL','2013-08-09','2013-08-09','2013-08-09',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7115,'AJUSTES EXPORTACAO LOSANGO','2013-08-19','2013-08-19','2013-08-19',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7127,'CRIAR PROCESSADOR COLETA CACIQUE','2013-08-19','2013-08-21','2013-08-21',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7136,'EXPORTACAO ITAU ADICIONAL','2013-08-21','2013-08-21','2013-08-21',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7140,'AJUSTE PROCESSADOR CUSTODIA MIDWAY','2013-08-22','2013-08-22','2013-08-22',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7101,'EMG ALTERAR LISTA DE POSTAGEM DETALHADA','2013-08-28','2013-09-03','2013-09-03',7,1);
+insert into ws_servico (os,titulo,dt_entrada,dt_homologacao,dt_commit,evento_id,codigo_usuario) values (7193,'REGRAS QUALICORP','2013-09-02','2013-09-03','2013-09-03',7,1);
+
+
+
+
+
+
+
