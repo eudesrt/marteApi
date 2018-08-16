33c62a6f1423f9bfb91e61f0866150ee60c25eb5
Your NameRafael <eudesrt@gmail.comm>
2018-08-16 20:04:14 -0300

    Melhorias

---
+++ src/main/java/com/br/marte/api/model/Status_.java
@@ -0,0 +1,16 @@
+package com.br.marte.api.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Status.class)
+public abstract class Status_ {
+
+	public static volatile SingularAttribute<Status, Long> codigo;
+	public static volatile SingularAttribute<Status, Integer> evento_id;
+	public static volatile SingularAttribute<Status, String> status;
+
+}
+
